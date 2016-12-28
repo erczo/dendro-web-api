df79f0fdc233df0d4e3c64b76b131be464bf5f94
J. Scott Smith <scott@newleafsolutionsinc.com>
2016-12-27 16:13:56 -0800

    Updated schemas, added additional type check for sort

--- src/server/services/datapoint/index.js
+++ src/server/services/datapoint/index.js
@@ -87,7 +87,7 @@ class Service {
 
     // Points can only be sorted by 'time' (default DESC)
     filters.$sort = {
-      time: typeof filters.$sort.time === 'undefined' ? -1 : filters.$sort.time
+      time: (typeof filters.$sort === 'object') && (typeof filters.$sort.time !== 'undefined') ? filters.$sort.time : -1
     }
     config = filters.$sort.time === -1 ? stack.reverse() : stack
 
