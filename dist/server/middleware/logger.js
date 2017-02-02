0bfd456de27a556eca9dd1448dcaa85bf73d0915
chris-wong-1 <wong.christopher.s@gmail.com>
2017-02-01 16:23:16 -0800

    Run build after updating schemas

--- dist/server/middleware/logger.js
+++ dist/server/middleware/logger.js
@@ -9,7 +9,7 @@ module.exports = app => {
 
   return (error, req, res, next) => {
     if (error) {
-      const message = `${ error.code ? `(${ error.code }) ` : '' }Route: ${ req.url } - ${ error.message }`;
+      const message = `${error.code ? `(${error.code}) ` : ''}Route: ${req.url} - ${error.message}`;
 
       if (error.code === 404) {
         winston.info(message);
